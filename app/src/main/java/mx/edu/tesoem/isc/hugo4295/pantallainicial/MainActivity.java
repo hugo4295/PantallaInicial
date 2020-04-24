a29a7ebe3c4e6bebf6308a5c8905de60c37ce4d7
hugo4295 <hugo_4295@hotmail.com>
2020-04-24 00:45:16 -0500

    Initial commit

---
+++ app/src/main/java/mx/edu/tesoem/isc/hugo4295/pantallainicial/MainActivity.java
@@ -0,0 +1,26 @@
+package mx.edu.tesoem.isc.hugo4295.pantallainicial;
+
+import androidx.annotation.MainThread;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,PrincipalActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 10000);
+    }
+}
